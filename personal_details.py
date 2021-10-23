f9a913b052db97a3169028360942b7dc0b9d82ca
lrglvz <laramarieisabelagalvez@iskolarngbayan.pup.edu.ph>
2021-10-23 20:12:23 +0800

    initial commit

---
+++ personal_details.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("Input your age: ")
+address = input("State your current address: ")
+print("Hi, my name is " + name + ". I am " + age + " years old and I live in " + address + ".")
